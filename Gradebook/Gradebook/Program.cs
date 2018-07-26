84209f3c7c343ccb964434ebd5caee9aea1ae31d
Kmylius <kayla.mylius@gmail.com>
2018-07-25 17:59:13 -0700

    Gradebook practice

---
+++ Gradebook/Gradebook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grades book = new Grades();
+            book.AddGrade(91f);
+            book.AddGrade(89.5f);
+            book.AddGrade(75f);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine("The average grade is " + stats.AverageGrade);
+            Console.WriteLine("The highest grade is " + stats.HighestGrade);
+            Console.WriteLine("The lowest grade is " + stats.LowestGrade);
+            Console.ReadLine();
+        }
+    }
+}
