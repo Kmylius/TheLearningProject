5747a838e927607611a5915f005107b8642832ed
Kmylius <kayla.mylius@gmail.com>
2018-07-31 20:11:05 -0700

    Linked Lists

---
+++ CollectIt/CollectIt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(3);
+            list.AddFirst(4);
+
+            var first = list.First;
+            list.AddBefore(first, 5);
+            list.AddAfter(first, 10);
+
+            var node = list.First;
+            while(node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ CollectIt/CollectIt/TestLinkList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectIt
+{
+    public class TestLinkList
+    {
+        public void Can_Add_After()
+        {
+            var list = new LinkedList<string>();
+            list.AddFirst("Hello");
+            list.AddLast("World");
+            list.AddAfter(list.First, "there");
+
+        }
+    }
+}
